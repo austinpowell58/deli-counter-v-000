46b6be02590381a502fda9887c1f93b7923fd268
Austin Powell <austinpowell58@gmail.com>
2018-08-08 23:24:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   else 
     counter = 1 
-    message = "The line is currently: "
+    message = "The line is currently:"
     katz_deli.each do |name|
-      message << "#{counter}. #{name}"
+      message << " #{counter}. #{name}"
       counter += 1
     end 
     puts message
